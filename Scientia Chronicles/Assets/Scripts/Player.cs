404482fbcb375d121c3c91498b19c1465d33678d
KaycPaulo <127959968+KaycPaulo@users.noreply.github.com>
2024-02-28 01:24:29 -0300

    tentativa de movimento 8 direções

--- Scientia Chronicles/Assets/Scripts/Player.cs
+++ Scientia Chronicles/Assets/Scripts/Player.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using UnityEngine.UIElements;
+using Vector2 = System.Numerics.Vector2;
+using Vector3 = System.Numerics.Vector3;
 
 public class Player : MonoBehaviour
 {
+
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,6 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
     }
 }
