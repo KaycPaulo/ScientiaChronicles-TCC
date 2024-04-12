f499cc77cddf85c3c438b7c08c6fa4687226d2c8
KaycPaulo <127959968+KaycPaulo@users.noreply.github.com>
2024-04-12 17:12:48 -0300

    melda

--- Scientia Chronicles/Assets/Scripts/Talker.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu (menuName ="Create Talker", fileName ="New talker")]
-public class Talker : ScriptableObject
-{
-    public Sprite _sprite;
-}
